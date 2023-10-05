9b65ac5e1c5b3d423e176d384825cc9e636fff43
HARVEYNASH\anhnguyens <sieunhantanbao@gmail.com>
2023-10-05 15:09:41 +0700

    Add terraform

---
+++ iac/terraform/aks/variables.tf
@@ -0,0 +1,40 @@
+variable "resource_group_location" {
+  default       = "southcentralus"
+  description   = "Location of the resource group."
+}
+
+variable "agent_count" {
+    default = 2
+}
+
+variable "ssh_public_key" {
+    default = "ssh_public_key"
+}
+
+variable "dns_prefix" {
+    default = "sd2411-my-todo-dns"
+}
+
+variable cluster_name {
+    default = "sd2411_k8s_cluster"
+}
+
+variable resource_group_name {
+    default = "sd2411"
+}
+
+variable locationk8s {
+    default = "southcentralus"
+}
+
+variable aks_service_principal_app_id {
+    default= "aks_service_principal_app_id"
+}
+
+variable aks_service_principal_client_secret{
+    default = "aks_service_principal_client_secret"
+}
+
+variable aks_service_principal_object_id {
+    default= "aks_service_principal_object_id"
+}
